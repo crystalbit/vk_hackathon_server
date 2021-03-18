a6156beef30415ff6bd81cc15376b0723f1ed678
Dmitrii Korotkin <d.korotkin@corp.vk.com>
2021-03-18 18:26:41 +0300

    try to deploy ws

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import * as express from 'express';
-import {IndexRouter} from "./routers/index.router";
+import { IndexRouter } from "./routers/index.router";
 import * as bodyParser from 'body-parser';
+import './services/websocket.service';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(IndexRouter);
 
-app.listen(process.env.PORT ?? 3000, () => {
+app.listen(process.env.PORT ?? 5000, () => {
   console.log('Server started');
 });
---
+++ src/services/websocket.service.ts
@@ -0,0 +1,28 @@
+import { createServer, Server as HTTPServer } from 'http';
+import * as express from 'express';
+import { Server } from 'socket.io';
+
+const app = express();
+
+const server = createServer(app);
+
+const io = new Server(server);
+
+server.listen(process.env.WS_PORT ?? 90, () => {
+  console.log('WS Started');
+});
+
+io.on('connect', (socket) => {
+  console.log('Connected client', socket.handshake.query);
+  setInterval(() => {
+    socket.send('pivo', { a: 5});
+  }, 2000);
+  socket.on('message', (m) => {
+    console.log('[server](message): %s', JSON.stringify(m));
+    io.emit('message', m);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
